f15edf2d679149a6cf73271a432078cf75f20217
malmeiser <malmeiser23@gmail.com>
2017-02-09 10:16:38 -0800

    Add files via upload

---
+++ operation_seattle.py
@@ -0,0 +1,35 @@
+print "Hanging out in Seattle! Mallory's First Python Game"
+
+import time
+
+time.sleep(1)
+
+name = raw_input("What's your name? ")
+
+time.sleep(1)
+
+print "Hey " + name + " what do you want to do today?"
+
+time.sleep (1)
+
+def choose_adventure(event):
+        options = raw_input("""
+Hang out at Pikes Place
+Waterfall Hike
+PieBar
+Get out of the city
+Lazy day inside
+""").lower()
+if 'pikes place' in options:
+    print hi
+else:
+    print test
+    
+print choose_adventure
+
+
+
+
+
+
+
